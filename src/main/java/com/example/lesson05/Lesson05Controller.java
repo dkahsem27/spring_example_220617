1fe70d5923bd0d83e0bf1986de147092bc7d777a
user <user>
2022-09-28 21:42:24 +0900

    0928 예제 - JSTL core 라이브러리

---
+++ src/main/java/com/example/lesson05/Lesson05Controller.java
@@ -0,0 +1,52 @@
+package com.example.lesson05;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/lesson05")
+@Controller
+public class Lesson05Controller {
+
+	@RequestMapping("/ex01")
+	public String ex01() {
+		return "lesson05/ex01";
+	}
+	
+	@GetMapping("/ex02")
+	public String ex02(Model model) {
+		// List<String>
+		List<String> fruits = new ArrayList<>();
+		fruits.add("melon");
+		fruits.add("apple");
+		fruits.add("pear");
+		fruits.add("mango");
+		fruits.add("watermelon");
+		
+		model.addAttribute("fruits", fruits);
+		
+		// List<Map>
+		List<Map<String, Object>> users = new ArrayList<>();
+		Map<String, Object> map = new HashMap<>();
+		map.put("name", "권예지");
+		map.put("age", 99);
+		map.put("hobby", "운동");
+		users.add(map);
+		
+		map = new HashMap<>();
+		map.put("name", "김뽀삐");
+		map.put("age", 16);
+		map.put("hobby", "짖기");
+		users.add(map);
+		
+		model.addAttribute("users", users);
+		
+		return "lesson05/ex02";
+	}
+}
